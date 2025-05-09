3eb3b57060f291b512fa5e6c378a2cc2595ee440
nortHades <128273140+nortHades@users.noreply.github.com>
2025-05-08 17:54:59 -0700

    new function User

---
+++ src/main/java/com/dom_cheung/ecommerce_store/model/User.java
@@ -0,0 +1,28 @@
+package com.dom_cheung.ecommerce_store.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean enabled = true;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+}
