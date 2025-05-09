42023e0e7d812a88fa8dc86bc95123b6a5ca5e25
nortHades <128273140+nortHades@users.noreply.github.com>
2025-05-08 17:56:04 -0700

    new function ProductController

--- src/main/java/com/dom_cheung/ecommerce_store/controller/ProductController.java
+++ src/main/java/com/dom_cheung/ecommerce_store/controller/ProductController.java
@@ -20,25 +20,25 @@ public class ProductController {
         return productRepository.findAll();//return all the products in the productRepo
     }
 
-    @PostMapping //<<< Annotation for handling HTTP POST requests to /api/products
-    public ResponseEntity<Product> createProduct(@RequestBody Product product){
-        //@RequestBody tells Spring to take the JSON sent in the request body
-        // and automatically convert it into a Product object.
-        System.out.println("====== METHOD CALLED: createProduct (ORIGINAL VERSION) ======");
-        try
-        {
-            // The save() method returns the saved entity (possibly with generated ID)
-            //insert into table products
-            Product savedProduct = productRepository.save(product);
-            //return the new response entity with saved product
-            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
-        }
-        catch(Exception e)
-        {
-            System.err.println(e.getMessage());//error with creating products
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+//    @PostMapping //<<< Annotation for handling HTTP POST requests to /api/products
+//    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+//        //@RequestBody tells Spring to take the JSON sent in the request body
+//        // and automatically convert it into a Product object.
+//        System.out.println("====== METHOD CALLED: createProduct (ORIGINAL VERSION) ======");
+//        try
+//        {
+//            // The save() method returns the saved entity (possibly with generated ID)
+//            //insert into table products
+//            Product savedProduct = productRepository.save(product);
+//            //return the new response entity with saved product
+//            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+//        }
+//        catch(Exception e)
+//        {
+//            System.err.println(e.getMessage());//error with creating products
+//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 
     @GetMapping("/{id}") // <<< Annotation for handling GET requests to /api/products/{id}
     public ResponseEntity<Product> getProductById(@PathVariable long id) {
@@ -57,40 +57,40 @@ public class ProductController {
         }
     }
 
-    @PutMapping("/{id}") // <<< Annotation for handling HTTP PUT requests to /api/products/{id}
-    public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
-        //The same with the get,check if the id exist
-        Optional<Product> productOp = productRepository.findById(id);
+//    @PutMapping("/{id}") // <<< Annotation for handling HTTP PUT requests to /api/products/{id}
+//    public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
+//        //The same with the get,check if the id exist
+//        Optional<Product> productOp = productRepository.findById(id);
+//
+//        if (productOp.isPresent()) {
+//            Product existingProduct = productOp.get();//set the product to the return product from repo
+//            existingProduct.setName(product.getName());
+//            existingProduct.setDescription(product.getDescription());
+//            existingProduct.setPrice(product.getPrice());
+//            existingProduct.setImageUrl(product.getImageUrl());
+//            //save the existingProduct to repo
+//            Product updatedProduct = productRepository.save(existingProduct);
+//            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+//        }
+//        else {
+//            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+//        }
+//    }
 
-        if (productOp.isPresent()) {
-            Product existingProduct = productOp.get();//set the product to the return product from repo
-            existingProduct.setName(product.getName());
-            existingProduct.setDescription(product.getDescription());
-            existingProduct.setPrice(product.getPrice());
-            existingProduct.setImageUrl(product.getImageUrl());
-            //save the existingProduct to repo
-            Product updatedProduct = productRepository.save(existingProduct);
-            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/{id}") // <<< Annotation for handling HTTP DELETE requests to /api/products/{id}
-    public ResponseEntity<Product> deleteProduct(@PathVariable long id) {
-        //get id from the request
-
-        //get Optional obj of product by find
-        Optional<Product> productOp = productRepository.findById(id);
-        //check the existing of product with specific id
-        if (productOp.isPresent()) {
-            Product productDeleted = productOp.get();
-            productRepository.delete(productOp.get());
-            return new ResponseEntity<>(productDeleted, HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
-    }
+//    @DeleteMapping("/{id}") // <<< Annotation for handling HTTP DELETE requests to /api/products/{id}
+//    public ResponseEntity<Product> deleteProduct(@PathVariable long id) {
+//        //get id from the request
+//
+//        //get Optional obj of product by find
+//        Optional<Product> productOp = productRepository.findById(id);
+//        //check the existing of product with specific id
+//        if (productOp.isPresent()) {
+//            Product productDeleted = productOp.get();
+//            productRepository.delete(productOp.get());
+//            return new ResponseEntity<>(productDeleted, HttpStatus.OK);
+//        }
+//        else {
+//            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+//        }
+//    }
 }
