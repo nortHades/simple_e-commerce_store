69bbbc31ed931b0c4d1f4d8a6515b2119f7ee996
nortHades <128273140+nortHades@users.noreply.github.com>
2025-05-08 17:55:53 -0700

    new function Product

--- src/main/java/com/dom_cheung/ecommerce_store/model/Product.java
+++ src/main/java/com/dom_cheung/ecommerce_store/model/Product.java
@@ -1,28 +1,33 @@
 package com.dom_cheung.ecommerce_store.model;
 
-import jakarta.persistence.*; //using jakarta.persistence for Spring Boot 3+
+import jakarta.persistence.*; // using jakarta.persistence for Spring Boot 3+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "products")
-@Data //This is to automaticly generate getter, setter, to String, Equals, hashCode
+@Data // This automatically generates getters, setters, toString, equals, and hashCode methods
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Product {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)//increment automatically
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment for the ID
     private long id;
 
     @Column(nullable = false)
     private String name;
 
+    @Column(columnDefinition = "TEXT") // Use TEXT for potentially longer descriptions
     private String description;
 
     @Column(nullable = false)
     private double price;
 
+    @Column(length = 512)
     private String imageUrl;
+
+    @Column(length = 255)
+    private String imagePublicId;
 }
